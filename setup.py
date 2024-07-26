377e05bad86f2793426d1bb942580405a621e291
djbios <dorandval@gmail.com>
2024-07-26 13:59:17 +0200

    versions

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='whatshouldido',
-    version='0.1.0',
+    version='0.1.10',
     py_modules=['whatshouldido'],
     install_requires=[
         'Click',
